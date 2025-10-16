1ecbdbaa76956c31b5d6efd231b79fdd96cd9d28
wingyin <wingyin3003@gmail.com>
2025-10-17 01:53:36 +0300

    updated email address

--- components/Misc/ScrollMore.tsx
+++ components/Misc/ScrollMore.tsx
@@ -103,7 +103,7 @@ const ScrollMore = () => {
               as="a"
               paddingY={3}
               fontFamily="monospace"
-              href="mailto:marcjhon18@gmail.com"
+              href="mailto:wingyin3003@gmail.com"
               target="_blank"
               rel="noreferrer"
               color={emailColor}
@@ -130,7 +130,7 @@ const ScrollMore = () => {
                 marginTop: '10px',
               }}
             >
-              marcjhon@gmail.com{' '}
+              wingyin3003@gmail.com{' '}
             </Text>
           </motion.div>
         )}
